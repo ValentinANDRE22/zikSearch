bdbaa47556f62bd5332ae6f16581adfaefca5b48
kingdom <a.manueljoao.aftec@gmail.com>
2017-11-16 11:07:29 +0100

    clean prject

--- app/src/main/java/activity/MainActivity.java
+++ app/src/main/java/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.mds.valentinandre.ziksearch;
+package activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.mds.valentinandre.ziksearch.R;
 
 public class MainActivity extends AppCompatActivity {
 
